ad25eb7057b49328a5012aac5a04547127d96b68
BestKung <best.kittiporn@gmail.com>
2015-05-23 17:03:44 +0700

    add user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
 INSERT INTO USERS(EMAIL,NAME,ENABLED,PASSWORD) values ('admin@admin.com','admin admin',1,'password')
+INSERT INTO USERS(EMAIL,NAME,ENABLED,PASSWORD) values ('Best@admin.com','Kittiporn',1,'best')
+INSERT INTO USERS(EMAIL,NAME,ENABLED,PASSWORD) values ('Nut@admin.com','Wasupa',1,'nut')
+INSERT INTO USERS(EMAIL,NAME,ENABLED,PASSWORD) values ('OOO@admin.com','Jasin',1,'ooo')
