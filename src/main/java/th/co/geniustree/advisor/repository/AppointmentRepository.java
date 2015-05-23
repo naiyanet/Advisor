c5365109b35c98a341e1d8fb16e45d9f5b82da33
momo <thangmo_thangmo@hotmail.com>
2015-05-23 16:42:47 +0700

    add repo appointment

---
+++ src/main/java/th/co/geniustree/advisor/repository/AppointmentRepository.java
@@ -0,0 +1,11 @@
+package th.co.geniustree.advisor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author User
+ */
+public interface AppointmentRepository extends JpaRepository<Appointment, String>{
+    
+}
--- src/main/java/th/co/geniustree/advisor/repository/UserRepository.java
+++ src/main/java/th/co/geniustree/advisor/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package th.co.geniustree.advisor.repository;
 
+import javax.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import th.co.geniustree.advisor.domain.User;
 
