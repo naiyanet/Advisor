a2969b4724d9730ae965751c48da6362cd0eae77
BestKung <best.kittiporn@gmail.com>
2015-05-23 17:10:53 +0700

    new Apploinment

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO USERS(EMAIL,NAME,ENABLED,PASSWORD) values ('admin@admin.com','admin admin',1,'password')
-INSERT INTO USERS(EMAIL,NAME,ENABLED,PASSWORD) values ('Best@admin.com','Kittiporn',1,'best')
-INSERT INTO USERS(EMAIL,NAME,ENABLED,PASSWORD) values ('Nut@admin.com','Wasupa',1,'nut')
-INSERT INTO USERS(EMAIL,NAME,ENABLED,PASSWORD) values ('OOO@admin.com','Jasin',1,'ooo')
+INSERT INTO Appoinment(starttime,endtime,detail,teacher,student) values (sysdate,sysdate,'geniustree','nut','ooooo')
+INSERT INTO Appoinment(starttime,endtime,detail,teacher,student) values (sysdate,sysdate,'geniustree','nut','Best')
+INSERT INTO Appoinment(starttime,endtime,detail,teacher,student) values (sysdate,sysdate,'geniustree','nut','nut')
+
