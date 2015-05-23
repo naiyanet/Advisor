25f24825ba4e5ca6112f5dae9e8abafbf09fdc86
momo <thangmo_thangmo@hotmail.com>
2015-05-23 11:49:28 +0700

    initail project

---
+++ src/main/java/advisor/AdvisorApplication.java
@@ -0,0 +1,12 @@
+package advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvisorApplication.class, args);
+    }
+}
---
+++ src/test/java/advisor/AdvisorApplicationTests.java
@@ -0,0 +1,18 @@
+package advisor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AdvisorApplication.class)
+@WebAppConfiguration
+public class AdvisorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
