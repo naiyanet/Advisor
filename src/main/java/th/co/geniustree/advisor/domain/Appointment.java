7a94f21e2543f3aca8e886fce5c202e5ca5b140b
pramoth <pramoth@geniustree.co.th>
2015-05-24 15:43:46 +0700

    fixed import

--- src/main/java/th/co/geniustree/advisor/domain/Appointment.java
+++ src/main/java/th/co/geniustree/advisor/domain/Appointment.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 
